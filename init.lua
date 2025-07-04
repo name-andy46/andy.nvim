50d440bc72f65fb3421e223df63f49237482b3bb
Anant Dongre <anant.dongre@shorelineiot.com>
2025-07-04 10:03:39 +0530

    Reduce scrolloff distance to 6 lines

--- init.lua
+++ init.lua
@@ -160,7 +160,7 @@ vim.o.cursorline = true
 vim.o.cursorcolumn = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 6
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
