3660a498d6cc85c9c743f6a87e10dc615a09a311
Anant Dongre <anant.dongre@shorelineiot.com>
2025-07-29 22:20:37 +0530

    Added cursor smear trail plugin

---
+++ lua/custom/plugins/smear.lua
@@ -0,0 +1,13 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    cursor_color = '#ff8800',
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    damping = 0.5,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+}
